076de299378c7aa92a4ada918291a1bba8c0ac45
DonizeteVida <donizetevida1998@outlook.com>
2025-12-14 23:10:39 +0100

    Filter turing device

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use serialport::SerialPort;
-use anyhow::Result;
+use anyhow::{Context, Result};
+use serialport::{SerialPort, SerialPortType};
 
 #[derive(Debug)]
 struct Display {
@@ -8,10 +8,21 @@ struct Display {
 
 impl Display {
     fn new() -> Result<Self> {
-        let mut available_ports = serialport::available_ports()?;
-        let port_info = available_ports.pop().unwrap();
+        let available_ports = serialport::available_ports()?;
+        let turing_device = available_ports
+            .iter()
+            .filter(|info| {
+                if let SerialPortType::UsbPort(ref info) = info.port_type {
+                    info.serial_number == Some("USB35INCHIPSV2".to_owned())
+                } else {
+                    false
+                }
+            })
+            .collect::<Vec<_>>()
+            .pop()
+            .context("Turing device not found")?;
 
-        let mut conn = serialport::new(port_info.port_name, 115_200).open()?;
+        let mut conn = serialport::new(turing_device.port_name.to_owned(), 115_200).open()?;
         conn.write_request_to_send(true)?;
 
         Ok(Self { conn })
